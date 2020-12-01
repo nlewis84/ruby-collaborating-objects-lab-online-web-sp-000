b676e80f5ebbe0d84ab4c8ac2a2b07c22d1ecfc1
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-01 03:11:52 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,9 +17,3 @@ class MP3Importer
     
   end
 end
-
-# ["Real Estate - Green Aisles - country.mp3",
-# "Real Estate - It's Real - hip-hop.mp3",
-# "Action Bronson - Larry Csonka - indie.mp3",
-# "Thundercat - For Love I Come - dance.mp3"]
-
